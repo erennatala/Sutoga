8aaeedcc66a4acd357ef48993d6e8ee5553e854b
Kerem Mican <47419589+keremmican@users.noreply.github.com>
2023-05-24 12:18:23 +0300

    [Kerem Mican] api fix

--- backend/backend/src/main/java/com/sutoga/backend/repository/CategoryRepository.java
+++ backend/backend/src/main/java/com/sutoga/backend/repository/CategoryRepository.java
@@ -3,8 +3,10 @@ package com.sutoga.backend.repository;
 import com.sutoga.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
-    Category findByName(String name);
+    Optional<Category> findByName(String name);
 
 }
