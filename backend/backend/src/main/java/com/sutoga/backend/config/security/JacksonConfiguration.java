1206a89f9ff84306df5f2c5d5ea886a615fdf63d
Kerem Mican <47419589+keremmican@users.noreply.github.com>
2023-05-25 14:20:31 +0300

    [Kerem Mican] socket fix

---
+++ backend/backend/src/main/java/com/sutoga/backend/config/security/JacksonConfiguration.java
@@ -0,0 +1,18 @@
+package com.sutoga.backend.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+}
--- backend/backend/src/main/java/com/sutoga/backend/service/impl/NotificationService.java
+++ backend/backend/src/main/java/com/sutoga/backend/service/impl/NotificationService.java
@@ -1,9 +1,12 @@
 package com.sutoga.backend.service.impl;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sutoga.backend.entity.Notification;
 import com.sutoga.backend.entity.User;
 import com.sutoga.backend.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,10 +17,17 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
     private final SocketIOServer server;
+    private final ObjectMapper objectMapper;
 
-    public NotificationService(NotificationRepository notificationRepository, SocketIOServer server) {
+    @Autowired
+    public NotificationService(NotificationRepository notificationRepository, SocketIOServer server, ObjectMapper objectMapper) {
         this.notificationRepository = notificationRepository;
         this.server = server;
+        this.objectMapper = objectMapper;
+    }
+
+    public NotificationService(NotificationRepository notificationRepository, SocketIOServer server) {
+        this(notificationRepository, server, new ObjectMapper());
     }
 
     public List<Notification> getNotifications(User user) {
@@ -28,7 +38,13 @@ public class NotificationService {
         notification.setCreatedAt(LocalDateTime.now());
         notificationRepository.save(notification);
         System.out.println("Sending notification: " + notification);
-        server.getBroadcastOperations().sendEvent("notification", notification);
+
+        try {
+            String jsonNotification = objectMapper.writeValueAsString(notification);
+            server.getBroadcastOperations().sendEvent("notification", jsonNotification);
+        } catch (JsonProcessingException e) {
+            System.out.println("gönderilemedi " + e);
+        }
     }
 
 }
