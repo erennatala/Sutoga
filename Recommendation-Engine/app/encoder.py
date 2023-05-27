1880c0d0ad75fd33f2afd54c401300c361b7f43c
erennatala <muhammeterenatala@gmail.com>
2023-05-27 10:06:37 +0300

    recommendation engine save

---
+++ Recommendation-Engine/app/encoder.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+""""# Read the game dataset from a CSV file
+game_dataset = pd.read_csv('/Users/eren/PycharmProjects/Sutoga/Recommendation-Engine/data/top_100_detailed.csv')
+# Extract the app_id, categories, genres, and publishers from the game dataset
+app_id = game_dataset['appid']
+categories = game_dataset['categories']
+genres = game_dataset['genres']
+publishers = game_dataset['publishers']
+
+# Process categories, genres, and publishers to extract the IDs
+categories_ids = categories.apply(lambda x: [category['id'] for category in eval(x)])
+genres_ids = genres.apply(lambda x: [genre['id'] for genre in eval(x)])
+publishers_ids = publishers.apply(lambda x: [x])
+
+# Perform one-hot encoding on category IDs
+category_encoder = MultiLabelBinarizer()
+category_encoding = category_encoder.fit_transform(categories_ids)
+
+# Perform one-hot encoding on genre IDs
+genre_encoder = MultiLabelBinarizer()
+genre_encoding = genre_encoder.fit_transform(genres_ids)
+
+# Perform one-hot encoding on publisher IDs
+publisher_encoder = MultiLabelBinarizer()
+publisher_encoding = publisher_encoder.fit_transform(publishers_ids)
+
+# Create a new DataFrame with app_id, encoded features
+encoded_df = pd.DataFrame({
+    'app_id': app_id,
+    'categories': category_encoding.tolist(),
+    'genres': genre_encoding.tolist(),
+    'publishers': publisher_encoding.tolist()
+})
+
+# Write the encoded features to a new CSV file
+encoded_df.to_csv('encoded_game_dataset.csv', index=False)"""
+
+
+# def encode_game_features():
+#     # Read the game dataset from a CSV file
+#     game_dataset = pd.read_csv('/Users/eren/PycharmProjects/Sutoga/Recommendation-Engine/data/top_100_detailed.csv')
+#
+#     # Process categories, genres, and publishers to extract the IDs
+#     categories_ids = game_dataset['categories'].apply(lambda x: [category['id'] for category in eval(x)])
+#     genres_ids = game_dataset['genres'].apply(lambda x: [genre['id'] for genre in eval(x)])
+#     publishers_ids = game_dataset['publishers'].apply(lambda x: [publisher for publisher in eval(x)])
+#
+#     # Perform one-hot encoding on category IDs, genre IDs, and publisher IDs
+#     mlb = MultiLabelBinarizer()
+#     category_encoding = pd.DataFrame(mlb.fit_transform(categories_ids), columns=mlb.classes_, index=game_dataset.index)
+#     genre_encoding = pd.DataFrame(mlb.fit_transform(genres_ids), columns=mlb.classes_, index=game_dataset.index)
+#     publisher_encoding = pd.DataFrame(mlb.fit_transform(publishers_ids), columns=mlb.classes_, index=game_dataset.index)
+#
+#     # Concatenate the one-hot encoded features
+#     encoded_features = pd.concat([category_encoding, genre_encoding, publisher_encoding], axis=1)
+#
+#     # Insert app_id column to the encoded features DataFrame
+#     encoded_features.insert(0, 'app_id', game_dataset['appid'])
+#
+#     # Save the DataFrame
+#     encoded_features.to_csv('encoded_game_dataset.csv', index=False)
+#
+# encode_game_features()
+
+
+import json
+import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
+
+def encode_game_features():
+    # Read the game dataset from a JSON file
+    with open('filtered_games.json', 'r') as file:
+        game_dataset = json.load(file)
+
+    # Process tags and publisher to extract the IDs
+    tags_ids = [entry["tags"] for entry in game_dataset]
+    publishers_ids = [entry["publisher"] for entry in game_dataset]
+
+    # Perform one-hot encoding on tag IDs and publisher IDs
+    mlb = MultiLabelBinarizer()
+    tags_encoding = pd.DataFrame(mlb.fit_transform(tags_ids), columns=mlb.classes_, index=range(len(game_dataset)))
+    publisher_encoding = pd.DataFrame(mlb.fit_transform(publishers_ids), columns=mlb.classes_, index=range(len(game_dataset)))
+
+    # Concatenate the one-hot encoded features
+    encoded_features = pd.concat([tags_encoding, publisher_encoding], axis=1)
+
+    # Insert appid column to the encoded features DataFrame
+    encoded_features.insert(0, 'appid', [entry["appid"] for entry in game_dataset])
+    encoded_features[tags]
+    # Save the DataFrame
+    encoded_features.to_csv('encoded_game_dataset2.csv', index=False)
+
+encode_game_features()
---
+++ Recommendation-Engine/app/recommendation_engine.py
@@ -0,0 +1,57 @@
+from app.collaborative_filtering import collaborative_filtering_recommendations
+from app.content_based import content_based_recommendations
+
+def convert_user_games_to_dict(user_games):
+    user_games_dict = {}
+    for game in user_games:
+        app_id = game['appid']
+        play_time = game['play_time']
+        user_games_dict[app_id] = play_time
+    return user_games_dict
+def hybrid_recommendations(user_games):
+    user_games_dict = convert_user_games_to_dict(user_games)
+    content_based_recs = content_based_recommendations(user_games_dict)
+
+    # Generate collaborative filtering recommendations
+    #collaborative_recs = collaborative_filtering_recommendations(user_games)
+
+    # Combine the recommendations or apply your hybrid logic
+    #combined_recs = combine_recommendations(content_based_recs, collaborative_recs)
+    combined_recs = []
+    return content_based_recs
+
+# def hybrid_recommendations(user_games):
+#     #user_games_dict = convert_user_games_to_dict(user_games)
+#
+#     # Generate content-based recommendations
+#     content_based_recs = content_based_recommendations(user_games)
+#
+#     # Generate collaborative filtering recommendations
+#     collaborative_recs = collaborative_filtering_recommendations(user_games)
+#
+#     # Filter content-based recommendations based on similarity scores
+#     filtered_content_based_recs = []
+#     similarity_threshold = 0.7
+#
+#     for rec in content_based_recs:
+#         if rec['similarity_score'] >= similarity_threshold:
+#             filtered_content_based_recs.append(rec)
+#         else:
+#             break
+#
+#     # Filter collaborative filtering recommendations based on estimated ratings
+#     filtered_collaborative_recs = []
+#     rating_threshold = 0.7
+#
+#     for rec in collaborative_recs:
+#         if rec['estimated_rating'] >= rating_threshold:
+#             filtered_collaborative_recs.append(rec)
+#         else:
+#             break
+#
+#     # Combine the filtered recommendations
+#     combined_recs = filtered_content_based_recs + filtered_collaborative_recs
+#
+#     return combined_recs
+
+
